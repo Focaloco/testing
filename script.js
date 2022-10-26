6724e2d590e08b66ccf42a305df99911a1f5643d
Focaloco <116742611+Focaloco@users.noreply.github.com>
2022-10-26 12:18:01 -0300

    Subo mis files

---
+++ script.js
@@ -0,0 +1,8 @@
+//let email = document.getElementsByTagName("input")
+
+//for(let i=0;i<email.length;i++){
+//    if(email[i]=="@"){
+//        console.log("es un mail valido")
+//        break
+//    }
+//}
